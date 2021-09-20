dc6ae0bc4f3fae31d0d495066f8248fc44d3a179
Chirayu Sharma <thechirayu02@gmail.com>
2021-09-20 16:59:43 +0530

    Implemented displaying data from database

--- app/src/main/java/com/example/android/otpverification/CatalogActivity.java
+++ app/src/main/java/com/example/android/otpverification/CatalogActivity.java
@@ -12,17 +12,69 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CatalogActivity extends AppCompatActivity {
+
+    private RecyclerView serviceRecyclerView;
+    private serviceAdapter adapter;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catalog_activity);
 
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("Services");
+
+        serviceRecyclerView = findViewById(R.id.services_recycler_view);
+        serviceRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<Service> options
+                = new FirebaseRecyclerOptions.Builder<Service>()
+                .setQuery(reference, Service.class)
+                .build();
+
+        adapter = new serviceAdapter(options);
+
+        serviceRecyclerView.setAdapter(adapter);
+
         getSupportActionBar().setTitle(Html.fromHtml("<font color=\"#ffffff\">" + getString(R.string.app_name) + "</font>"));
         getSupportActionBar().setElevation(0);
 
+    }
+
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     @Override
